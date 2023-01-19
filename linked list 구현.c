264d4ce8d653eb1ae0b9f2b8a7ed6bc51b5deba6
woukl22 <woukl22@gmail.com>
2023-01-19 14:49:12 +0900

    Update code

--- linked list 구현.c
+++ linked list 구현.c
@@ -6,6 +6,7 @@ typedef struct _node{
 	struct _node* nextNode;
 }Node;
 
+
 Node* create_Node(int data){
 	Node* newNode = (Node*)malloc(sizeof(Node));
 	newNode->data = data;
@@ -18,42 +19,49 @@ void destroy_Node(Node* node){
 	free(node);
 }
 
+
 void append_Node(Node** head, int data){
 	Node* newNode = create_Node(data);
 	if(*head == NULL){
 		*head = newNode;
 	} else{
-		Node* tail = *head;
-		while(tail->nextNode != NULL){
-			tail = tail->nextNode;
+		Node* currentNode = *head;
+		while(currentNode->nextNode != NULL){
+			currentNode = currentNode->nextNode;
 		}
-		tail->nextNode = newNode;
+		currentNode->nextNode = newNode;
 	}
 }
 
 Node* find_Node(Node* head, int location){
 	Node* currentNode = head;
-	while(currentNode != NULL && (--location) >= 0){
+	while(currentNode != NULL && (--location) > 0){
 		currentNode = currentNode->nextNode;
 	}
-	if(location >= 0){
-		printf("Not found");
+	if(location > 0){
+		printf("해당 위치에 노드가 존재하지 않아, 헤드 노드를 반환합니다.\n");
+		return head;
 	}
 	return currentNode;
 }
 
-void remove_Node(Node* head, Node* removeNode){
-	Node* currentNode = head;
-	while(currentNode != NULL && currentNode->nextNode != removeNode){
-		currentNode = currentNode->nextNode;	
+void remove_Node(Node** head, Node* removeNode){
+	if(*head == removeNode){
+		*head = removeNode->nextNode;
+		destroy_Node(removeNode);
+	} else{
+		Node* currentNode = *head;
+		while(currentNode != NULL && currentNode->nextNode != removeNode){
+			currentNode = currentNode->nextNode;
+		}
+		if(currentNode != NULL){
+			currentNode->nextNode = removeNode->nextNode;	
+			destroy_Node(removeNode);
+		}
 	}
-	if(currentNode != NULL){
-		currentNode->nextNode = removeNode->nextNode;	
-	}
-	destroy_Node(removeNode);
 }
 
-void insertAfter_Node(Node* currentNode, int data){
+void insertNext_Node(Node* currentNode, int data){
 	Node* newNode = create_Node(data);
 	newNode->nextNode = currentNode->nextNode;
 	currentNode->nextNode = newNode;
@@ -68,22 +76,15 @@ int num_Node(Node* head){
 	return num;
 }
 
-int findData_Node(Node* head, int location){
-	Node* currentNode = head;
-	while(location > 0){
-		currentNode = currentNode->nextNode;
-		location--;
-	}
-	return currentNode->data;
-}
-
 void print_List(Node* head){
 	Node* currentNode = head;
 	while(currentNode != NULL){
 		printf("%d ", currentNode->data);
 		currentNode = currentNode->nextNode;
 	}
+	printf("\n");
 }
+
 
 int main(void){
 	Node* list = NULL;
@@ -92,26 +93,24 @@ int main(void){
 	append_Node(&list, 2);
 	append_Node(&list, 3);
 	Node* find = find_Node(list, 2);
-	remove_Node(list, find);
-	find = find_Node(list, 1);
-	insertAfter_Node(find, 5);
+	remove_Node(&list, find);
+	print_List(list);	
+	find = find_Node(list, 7);
+	print_List(list);	
+	remove_Node(&list, find);
+	print_List(list);	
+	insertNext_Node(find, 5);
+	print_List(list);	
 	find = find_Node(list, 2);
+	print_List(list);	
 	int num = num_Node(list);
-	printf("리스트의 크기는 %d 입니다.\n", num);
-	int idx = findData_Node(list, 2);
-	printf("2번 인덱스에 들어있는 값은 %d 입니다.\n", idx);
-	printf("리스트에 있는 값은 다음과 같습니다: ");
-	print_List(list);
+	printf("리스트의 길이는 %d 입니다.\n", num);
+	print_List(list);	
+	
 	
 	while(num_Node != 0){
-		remove_Node(list, 0);
+		remove_Node(&list, 0);
 	}
 	destroy_Node(list);
 	return 0;	
 }
-
-
-
-
-
-
